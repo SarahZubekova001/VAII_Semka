4f1e21546e4f44dd2eaebf04af7f1c786f8a92da
Sarah <zubekova4@stud.uniza.sk>
2025-02-06 16:57:05 +0100

    preklep v nazve

--- vaiicko-master/App/Views/Auth/register.view.php
+++ vaiicko-master/App/Views/Auth/register.view.php
@@ -5,7 +5,7 @@ $layout = 'auth';
 ?>
 
 <!DOCTYPE html>
-prid<html lang="sk">
+<html lang="sk">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
